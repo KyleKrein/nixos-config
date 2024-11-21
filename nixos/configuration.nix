2191a27a346fc7da352f789cb4f6e2262b0f15e9
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2024-11-21 20:08:03 +0100

    Added bluetooth. Updated nixvim config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -218,7 +218,17 @@
 		enable = true;
 	};
 	logitech.wireless.enable = true;
+	bluetooth = {
+	    enable = true;
+	    powerOnBoot = true;
+	    settings = {
+		General = {
+		    Experimental = true;
+		};
+	    };
+	};
   };
+  services.blueman.enable = true;
 
   security.polkit.enable = true;
 
--- nixos/nixvim/default.nix
+++ nixos/nixvim/default.nix
@@ -8,7 +8,72 @@
 	enable = true;
 	colorschemes.catppuccin.enable = true;
 	plugins = {
-
+	    cmp = {
+		enable = true;
+		autoEnableSources = true;
+		settings = {
+		sources = [
+		    {
+            name = "nvim_lsp";
+            priority = 1000;
+            option = {
+              #inherit get_bufnrs;
+            };
+          }
+          {
+            name = "nvim_lsp_signature_help";
+            priority = 1000;
+            option = {
+              #inherit get_bufnrs;
+            };
+          }
+          {
+            name = "nvim_lsp_document_symbol";
+            priority = 1000;
+            option = {
+             # inherit get_bufnrs;
+            };
+          }
+          {
+            name = "treesitter";
+            priority = 850;
+            option = {
+            #  inherit get_bufnrs;
+            };
+          }
+          {
+            name = "buffer";
+            priority = 500;
+            option = {
+           #   inherit get_bufnrs;
+            };
+          }
+          {
+            name = "path";
+            priority = 300;
+          }
+		]; };
+	    };
+	    lsp-format = {
+		enable = true;
+	    };
+	    lsp = {
+		enable = true;
+		inlayHints = true;
+		servers = { 
+		    nixd = {
+          enable = true;
+          extraOptions = {
+            nixos = {
+              expr = "(builtins.getFlake \"/etc/nixos\").nixosConfigurations.aurelionite.options";
+            };
+            home_manager = {
+              expr = "(builtins.getFlake \"/etc/nixos\").homeConfigurations.aurelionite.options";
+            };
+          };
+        };
+		};
+	    };
 	};
 	opts = {
 	    number = true;
