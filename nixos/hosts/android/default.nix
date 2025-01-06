03486e3312dc7bc5fe67d198ab551d0bd105b243
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-01-06 20:41:21 +0100

    small fix

--- nixos/hosts/android/default.nix
+++ nixos/hosts/android/default.nix
@@ -1,10 +1,30 @@
-{ pkgs, ... }:
-
+{ pkgs, inputs, ... }:
 {
+    imports = [
+	inputs.nixvim.nixosModules.nixvim
+    ];
+    programs.nixvim = {
+	enable = true;
+	defaultEditor = true;
+
+	colorschemes.catppuccin.enable = true;
+	plugins = {
+		lualine.enable = true;
+	};
+
+	opts = {
+		number = true;
+		shiftwidth = 4;
+	};
+    };
     environment.packages = with pkgs;[
 	neovim
 	git
 	fastfetch
+
+	(pkgs.writeShellScriptBin "droid-switch" ''
+	nix-on-droid switch --flake /data/data/com.termux.nix/files/home/nixos-config
+	'')
     ];
     
     home-manager = {
