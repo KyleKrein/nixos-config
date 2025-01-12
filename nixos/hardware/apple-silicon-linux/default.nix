0421fb57b311ae226f37cb80c3d57fba2e332f57
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-01-12 15:59:08 +0100

    Enabled overlay for mac

--- nixos/hardware/apple-silicon-linux/default.nix
+++ nixos/hardware/apple-silicon-linux/default.nix
@@ -1,32 +1,33 @@
-{ pkgs, lib, ... }:
-let
-in
-{	
-	#nixpkgs.overlays = [
-	#    (import ./widevine-overlay.nix)
-	#];
-	#nixpkgs.config.allowUnsupportedSystem = true;
-	imports = [
-		#./apple-silicon-support
-	];
-	hardware.asahi = {
-		peripheralFirmwareDirectory = ./firmware;
-		useExperimentalGPUDriver = true;
-		#experimentalGPUInstallMode = "overlay";
-		setupAsahiSound = true;
-	};
+{
+  pkgs,
+  lib,
+  ...
+}: let
+in {
+  #nixpkgs.overlays = [
+  #    (import ./widevine-overlay.nix)
+  #];
+  #nixpkgs.config.allowUnsupportedSystem = true;
+  imports = [
+    #./apple-silicon-support
+  ];
+  hardware.asahi = {
+    peripheralFirmwareDirectory = ./firmware;
+    useExperimentalGPUDriver = true;
+    experimentalGPUInstallMode = "overlay";
+    setupAsahiSound = true;
+  };
 
-	#powerManagement = {
-	#    enable = true;
-	#    powertop.enable = true;
-	#};
-	#hardware.graphics.enable32Bit = lib.mkForce false;
-	environment.systemPackages = with pkgs; [
-	    #mesa
-	    #mesa.drivers
-	    mesa-asahi-edge
-	    #widevine-cdm 
-	    #widevinecdm-aarch64
-	];
-
+  #powerManagement = {
+  #    enable = true;
+  #    powertop.enable = true;
+  #};
+  #hardware.graphics.enable32Bit = lib.mkForce false;
+  environment.systemPackages = with pkgs; [
+    #mesa
+    #mesa.drivers
+    mesa-asahi-edge
+    #widevine-cdm
+    #widevinecdm-aarch64
+  ];
 }
