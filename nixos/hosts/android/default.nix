6e8277b6bead249a09660bf406daf64b97ab107e
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-01-18 08:23:12 +0100

    Splitted packages to fix issue with emacs on android

--- nixos/hosts/android/default.nix
+++ nixos/hosts/android/default.nix
@@ -4,20 +4,25 @@
   ...
 }: {
   imports = [
-    ../../modules/emacs
   ];
-  environment.packages = with pkgs; [
-    neovim
-    git
-    fastfetch
-    asciiquarium
-    cmatrix
-    inputs.neovim.packages.aarch64-linux.default
+  environment.packages = with pkgs;
+    [
+      neovim
+      git
+      fastfetch
+      asciiquarium
+      cmatrix
+      inputs.neovim.packages.aarch64-linux.default
 
-    (pkgs.writeShellScriptBin "droid-switch" ''
-      nix-on-droid switch --flake /data/data/com.termux.nix/files/home/nixos-config
-    '')
-  ];
+      (pkgs.writeShellScriptBin "droid-switch" ''
+        nix-on-droid switch --flake /data/data/com.termux.nix/files/home/nixos-config
+      '')
+    ]
+    ++ (import ../../modules/emacs/packages.nix {
+      inherit pkgs;
+      emacs = pkgs.emacs;
+    })
+    .packages;
   home-manager = {
     config = ./home.nix;
     useGlobalPkgs = true;
--- nixos/modules/emacs/default.nix
+++ nixos/modules/emacs/default.nix
@@ -5,53 +5,19 @@
   inputs,
   ...
 }: let
-  emacs = pkgs.emacs;#pkgs.emacs-pgtk;
+  emacs = pkgs.emacs; #pkgs.emacs-pgtk;
 in {
   services.emacs.enable = true;
   services.emacs.startWithGraphical = true;
   services.emacs.install = true;
   services.emacs.package = emacs; #pkgs.emacs-unstable
 
-  environment.systemPackages = with pkgs; [
-    git
-    libvterm
-    libtool
-    emacs
-    ripgrep
-    fd
-    coreutils
-    clang
-    cmake
-    nixfmt-rfc-style
-    markdownlint-cli
-    pandoc
-    gnumake
-    python3
-    isort
-    pipenv
-    python313Packages.nose2
-    python313Packages.pytest
-    graphviz
-    shellcheck
-    nodejs_23
-
-    (pkgs.writeShellScriptBin "doom-install" ''
-      ${pkgs.git}/bin/git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
-      ${pkgs.git}/bin/git clone https://github.com/KyleKrein/doomemacs.git ~/.doom.d
-      ~/.emacs.d/bin/doom install
-      ~/.emacs.d/bin/doom sync
-      pidof emacs || ${emacs}/bin/emacs --daemon &
-    '')
-    (pkgs.writeShellScriptBin "doom-sync" ''
-      ~/.emacs.d/bin/doom sync --aot --force
-    '')
-    (pkgs.writeShellScriptBin "doom-upgrade" ''
-      ~/.emacs.d/bin/doom upgrade
-    '')
-    (pkgs.writeShellScriptBin "doom" ''
-      ${emacs}/bin/emacsclient -c -a "${emacs}/bin/emacs"
-    '')
-  ];
+  environment.systemPackages =
+    (import ./packages.nix {
+      inherit pkgs;
+      inherit emacs;
+    })
+    .packages;
   nixpkgs.overlays = [
     inputs.emacs-overlay.overlays.default
   ];
