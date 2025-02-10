39db7ee6baf25dec572dd9b5ede26234e5020710
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-02-10 17:13:18 +0100

    Made steam non flatpak (cz Flatpaks bed)

--- nixos/modules/steam/default.nix
+++ nixos/modules/steam/default.nix
@@ -12,14 +12,15 @@ in {
   };
 
   config = lib.mkIf cfg.enable (
-    if hwconfig.useImpermanence
-    then {
-      kk.services.flatpak.enable = lib.mkForce true;
-      services.flatpak.packages = ["com.valvesoftware.Steam"];
-    }
-    else {
+   # if hwconfig.useImpermanence
+   # then {
+   #   kk.services.flatpak.enable = lib.mkForce true;
+   #   services.flatpak.packages = ["com.valvesoftware.Steam"];
+   # }
+   # else 
+    {
       programs.steam = {
-        enable = !hwconfig.useImpermanence;
+        enable = true;#!hwconfig.useImpermanence;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
