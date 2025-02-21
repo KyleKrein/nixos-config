ab8343948c9f84817ac2fdcc65f6b1e4850d4e27
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-02-21 19:31:52 +0100

    Updated

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,7 +140,7 @@
       ${pkgs.fd}/bin/fd --one-file-system --base-directory / --type f --hidden --exclude "{tmp,etc/passwd}"
     '') # https://www.reddit.com/r/NixOS/comments/1d1apm0/comment/l5tgbwz/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     gparted
-    freetube
+    #freetube
     exfatprogs
     kitty
     yazi
