05959315a02f3292fa4378e24d35588b1b7d84bf
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-02-21 12:39:00 +0100

    Fixed steam on 24.11

--- nixos/modules/steam/default.nix
+++ nixos/modules/steam/default.nix
@@ -3,6 +3,7 @@
   lib,
   config,
   hwconfig,
+  unstable-pkgs,
   ...
 }: let
   cfg = config.kk.steam;
@@ -24,6 +25,7 @@ in {
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
         localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+	package = unstable-pkgs.steam;
       };
     }
   );
