2401ca2add1fda4d18e0ee0679bf2679bcff1ec1
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-04-17 11:46:27 +0200

    Changed Conduwuit provider

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
     beeengine = {
       url = "git+https://github.com/KyleKrein/BeeEngine?submodules=1";
     };
-    conduwuit.url = "github:girlbossceo/conduwuit";
+    conduwuit.url = "github:matrix-construct/tuwunel";
   };
 
   outputs = {
