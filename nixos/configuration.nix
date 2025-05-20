e2f0d2e60da35ad4ed14de17205062afa3948f1d
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-05-20 21:29:38 +0200

    PDF Tool

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,6 +143,7 @@ in
       ${pkgs.fd}/bin/fd --one-file-system --base-directory / --type f --hidden --exclude "{tmp,etc/passwd}"
     '') # https://www.reddit.com/r/NixOS/comments/1d1apm0/comment/l5tgbwz/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
     gparted
+    qdirstat
     #freetube
     exfatprogs
     kitty
