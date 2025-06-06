df25453df0957f3c1320b9a73a16b6b0fdfad3f6
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-06-06 16:54:54 +0200

    Disabled plymouth

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,26 +42,10 @@ in
 
   boot = {
     kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-
-    plymouth = {
-      enable = true;
-    };
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = if hwconfig.hostname != "kylekrein-mac" then true else false;
     };
-    # Enable "Silent Boot"
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
